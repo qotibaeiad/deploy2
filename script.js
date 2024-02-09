e5fc0123f8078bd58999d197b4ed98e9cbdd74a1
qotibamhamed <qotibaeiad98@gmail.com>
2024-02-10 01:03:27 +0200

    s

--- script.js
+++ script.js
@@ -11,89 +11,86 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 // start dark mode
-
 const HSThemeAppearance = {
   init() {
-      const defaultTheme = 'default'
-      let theme = localStorage.getItem('hs_theme') || defaultTheme
+    const defaultTheme = 'default';
+    let theme = localStorage.getItem('hs_theme') || defaultTheme;
 
-      if (document.querySelector('html').classList.contains('dark')) return
-      this.setAppearance(theme)
+    if (document.querySelector('html').classList.contains('dark')) return;
+    this.setAppearance(theme);
   },
   _resetStylesOnLoad() {
-      const $resetStyles = document.createElement('style')
-      $resetStyles.innerText = `*{transition: unset !important;}`
-      $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
-      document.head.appendChild($resetStyles)
-      return $resetStyles
+    const $resetStyles = document.createElement('style');
+    $resetStyles.innerText = `*{transition: unset !important;}`;
+    $resetStyles.setAttribute('data-hs-appearance-onload-styles', '');
+    document.head.appendChild($resetStyles);
+    return $resetStyles;
   },
   setAppearance(theme, saveInStore = true, dispatchEvent = true) {
-      const $resetStylesEl = this._resetStylesOnLoad()
+    const $resetStylesEl = this._resetStylesOnLoad();
 
-      if (saveInStore) {
-          localStorage.setItem('hs_theme', theme)
-      }
+    if (saveInStore) {
+      localStorage.setItem('hs_theme', theme);
+    }
 
-      if (theme === 'auto') {
-          theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-      }
+    document.querySelector('html').classList.remove('dark');
+    document.querySelector('html').classList.remove('default');
+    document.querySelector('html').classList.remove('auto');
 
-      document.querySelector('html').classList.remove('dark')
-      document.querySelector('html').classList.remove('default')
-      document.querySelector('html').classList.remove('auto')
+    document.querySelector('html').classList.add(this.getOriginalAppearance());
 
-      document.querySelector('html').classList.add(this.getOriginalAppearance())
+    setTimeout(() => {
+      $resetStylesEl.remove();
+    });
 
-      setTimeout(() => {
-          $resetStylesEl.remove()
-      })
-
-      if (dispatchEvent) {
-          window.dispatchEvent(new CustomEvent('on-hs-appearance-change', {detail: theme}))
-      }
+    if (dispatchEvent) {
+      window.dispatchEvent(new CustomEvent('on-hs-appearance-change', { detail: theme }));
+    }
   },
   getAppearance() {
-      let theme = this.getOriginalAppearance()
-      if (theme === 'auto') {
-          theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-      }
-      return theme
+    let theme = this.getOriginalAppearance();
+    if (theme === 'auto') {
+      theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default';
+    }
+    return theme;
   },
   getOriginalAppearance() {
-      const defaultTheme = 'default'
-      return localStorage.getItem('hs_theme') || defaultTheme
+    const defaultTheme = 'default';
+    return localStorage.getItem('hs_theme') || defaultTheme;
   }
-}
-HSThemeAppearance.init()
+};
+
+HSThemeAppearance.init();
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
   if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
-      HSThemeAppearance.setAppearance('auto', false)
+    HSThemeAppearance.setAppearance('auto', false);
   }
-})
+});
 
 window.addEventListener('load', () => {
-  const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
-  const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
+  const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]');
+  const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]');
 
   $clickableThemes.forEach($item => {
-      $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item))
-  })
+    $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item));
+  });
 
   $switchableThemes.forEach($item => {
-      $item.addEventListener('change', (e) => {
-          HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
-      })
+    $item.addEventListener('change', (e) => {
+      HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default');
+    });
 
-      $item.checked = HSThemeAppearance.getAppearance() === 'dark'
-  })
+    $item.checked = HSThemeAppearance.getAppearance() === 'dark';
+  });
 
   window.addEventListener('on-hs-appearance-change', e => {
-      $switchableThemes.forEach($item => {
-          $item.checked = e.detail === 'dark'
-      })
-  })
-})
+    $switchableThemes.forEach($item => {
+      $item.checked = e.detail === 'dark';
+    });
+  });
+});
+
 
 
 // end dark mode
