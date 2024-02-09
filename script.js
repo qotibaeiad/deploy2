9d7c3e101be2688c4b6050bd39ddf0e8e68bc40f
qotibamhamed <qotibaeiad98@gmail.com>
2024-02-10 01:47:38 +0200

    dark mode

--- script.js
+++ script.js
@@ -10,139 +10,139 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdownMenu.classList.toggle('hidden');
   });
 
-// darkmode start youtube
+// // darkmode start youtube
 
-const sunIcon = document.querySelector(".sun");
-const moonIcon = document.querySelector(".moon");
+// const sunIcon = document.querySelector(".sun");
+// const moonIcon = document.querySelector(".moon");
 
-const userTheme = localStorage.getItem("theme");
-const systemTheme = window.matchMedia(("prefer-color-scheme: dark")).matches;
-
-
-const iconToggle = ()=>{
-    moonIcon.classList.toggle("display-none");
-    sunIcon.classList.toggle("display-none");
-};
+// const userTheme = localStorage.getItem("theme");
+// const systemTheme = window.matchMedia(("prefer-color-scheme: dark")).matches;
 
 
-const themCheck = () => {
-    if(userTheme ==="dark" || (!userTheme && systemTheme)){
-        document.documentElement.classList.add("dark");
-        moonIcon.classList.add("display-none");
-        return;
-    }
-    sunIcon.classList.add("display-none");
-};
+// const iconToggle = ()=>{
+//     moonIcon.classList.toggle("display-none");
+//     sunIcon.classList.toggle("display-none");
+// };
 
-const themSwitch = () =>{
-    if (document.documentElement.classList.contains("dark")){
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme","light");
-        iconToggle();
-        return;
-    }
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme","dark");
-    iconToggle();
-};
 
-sunIcon.addEventListener("click",()=>{
-    themSwitch();
-});
+// const themCheck = () => {
+//     if(userTheme ==="dark" || (!userTheme && systemTheme)){
+//         document.documentElement.classList.add("dark");
+//         moonIcon.classList.add("display-none");
+//         return;
+//     }
+//     sunIcon.classList.add("display-none");
+// };
 
-moonIcon.addEventListener("click",()=>{
-    themSwitch();
-});
+// const themSwitch = () =>{
+//     if (document.documentElement.classList.contains("dark")){
+//         document.documentElement.classList.remove("dark");
+//         localStorage.setItem("theme","light");
+//         iconToggle();
+//         return;
+//     }
+//     document.documentElement.classList.add("dark");
+//     localStorage.setItem("theme","dark");
+//     iconToggle();
+// };
 
-themCheck();
+// sunIcon.addEventListener("click",()=>{
+//     themSwitch();
+// });
 
-// darkmode end youtube
+// moonIcon.addEventListener("click",()=>{
+//     themSwitch();
+// });
+
+// themCheck();
+
+// // darkmode end youtube
 
 
 // start dark mode
 
-// const HSThemeAppearance = {
-//   init() {
-//       const defaultTheme = 'default'
-//       let theme = localStorage.getItem('hs_theme') || defaultTheme
+const HSThemeAppearance = {
+  init() {
+      const defaultTheme = 'default'
+      let theme = localStorage.getItem('hs_theme') || defaultTheme
 
-//       if (document.querySelector('html').classList.contains('dark')) return
-//       this.setAppearance(theme)
-//   },
-//   _resetStylesOnLoad() {
-//       const $resetStyles = document.createElement('style')
-//       $resetStyles.innerText = `*{transition: unset !important;}`
-//       $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
-//       document.head.appendChild($resetStyles)
-//       return $resetStyles
-//   },
-//   setAppearance(theme, saveInStore = true, dispatchEvent = true) {
-//       const $resetStylesEl = this._resetStylesOnLoad()
+      if (document.querySelector('html').classList.contains('dark')) return
+      this.setAppearance(theme)
+  },
+  _resetStylesOnLoad() {
+      const $resetStyles = document.createElement('style')
+      $resetStyles.innerText = `*{transition: unset !important;}`
+      $resetStyles.setAttribute('data-hs-appearance-onload-styles', '')
+      document.head.appendChild($resetStyles)
+      return $resetStyles
+  },
+  setAppearance(theme, saveInStore = true, dispatchEvent = true) {
+      const $resetStylesEl = this._resetStylesOnLoad()
 
-//       if (saveInStore) {
-//           localStorage.setItem('hs_theme', theme)
-//       }
+      if (saveInStore) {
+          localStorage.setItem('hs_theme', theme)
+      }
 
-//       if (theme === 'auto') {
-//           theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-//       }
+      if (theme === 'auto') {
+          theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+      }
 
-//       document.querySelector('html').classList.remove('dark')
-//       document.querySelector('html').classList.remove('default')
-//       document.querySelector('html').classList.remove('auto')
+      document.querySelector('html').classList.remove('dark')
+      document.querySelector('html').classList.remove('default')
+      document.querySelector('html').classList.remove('auto')
 
-//       document.querySelector('html').classList.add(this.getOriginalAppearance())
+      document.querySelector('html').classList.add(this.getOriginalAppearance())
 
-//       setTimeout(() => {
-//           $resetStylesEl.remove()
-//       })
+      setTimeout(() => {
+          $resetStylesEl.remove()
+      })
 
-//       if (dispatchEvent) {
-//           window.dispatchEvent(new CustomEvent('on-hs-appearance-change', {detail: theme}))
-//       }
-//   },
-//   getAppearance() {
-//       let theme = this.getOriginalAppearance()
-//       if (theme === 'auto') {
-//           theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
-//       }
-//       return theme
-//   },
-//   getOriginalAppearance() {
-//       const defaultTheme = 'default'
-//       return localStorage.getItem('hs_theme') || defaultTheme
-//   }
-// }
-// HSThemeAppearance.init()
+      if (dispatchEvent) {
+          window.dispatchEvent(new CustomEvent('on-hs-appearance-change', {detail: theme}))
+      }
+  },
+  getAppearance() {
+      let theme = this.getOriginalAppearance()
+      if (theme === 'auto') {
+          theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default'
+      }
+      return theme
+  },
+  getOriginalAppearance() {
+      const defaultTheme = 'default'
+      return localStorage.getItem('hs_theme') || defaultTheme
+  }
+}
+HSThemeAppearance.init()
 
-// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-//   if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
-//       HSThemeAppearance.setAppearance('auto', false)
-//   }
-// })
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
+      HSThemeAppearance.setAppearance('auto', false)
+  }
+})
 
-// window.addEventListener('load', () => {
-//   const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
-//   const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
+window.addEventListener('load', () => {
+  const $clickableThemes = document.querySelectorAll('[data-hs-theme-click-value]')
+  const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]')
 
-//   $clickableThemes.forEach($item => {
-//       $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item))
-//   })
+  $clickableThemes.forEach($item => {
+      $item.addEventListener('click', () => HSThemeAppearance.setAppearance($item.getAttribute('data-hs-theme-click-value'), true, $item))
+  })
 
-//   $switchableThemes.forEach($item => {
-//       $item.addEventListener('change', (e) => {
-//           HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
-//       })
+  $switchableThemes.forEach($item => {
+      $item.addEventListener('change', (e) => {
+          HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default')
+      })
 
-//       $item.checked = HSThemeAppearance.getAppearance() === 'dark'
-//   })
+      $item.checked = HSThemeAppearance.getAppearance() === 'dark'
+  })
 
-//   window.addEventListener('on-hs-appearance-change', e => {
-//       $switchableThemes.forEach($item => {
-//           $item.checked = e.detail === 'dark'
-//       })
-//   })
-// })
+  window.addEventListener('on-hs-appearance-change', e => {
+      $switchableThemes.forEach($item => {
+          $item.checked = e.detail === 'dark'
+      })
+  })
+})
 
 
 // end dark mode
