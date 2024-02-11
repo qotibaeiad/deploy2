8c929e4ac00cac6c2d7242c65ec95025d411596a
AbdallhAmarya <abdallh.amarya38@gmail.com>
2024-02-11 23:29:31 +0200

    pull request

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function checkLogin() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   if (username === 'abdallh' && password === '123456') {
-    window.location.href = 'https://www.ynet.co.il/home/0,7340,L-8,00.html';
+    window.location.href = 'index.html';
   } else {
     alert('Incorrect username or password');
   }
