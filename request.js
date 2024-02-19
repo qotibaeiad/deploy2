c7b59cf0adbded43870798a88b8d069286cbe17a
user <user@users-MacBook-Pro.local>
2024-02-19 19:48:44 +0200

    add spin loading

--- request.js
+++ request.js
@@ -58,6 +58,9 @@ function fetchAndDisplayArticles(category) {
           if (article.title && article.description && article.urlToImage) {
           const articleElement = document.createElement('div');
           articleElement.innerHTML = `
+          <div id="loadingSpinner" class="flex items-center justify-center h-screen fixed top-0 left-0 w-full bg-gray-800 bg-opacity-75 z-50 hidden">
+          <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+        </div>
           <div class="hover:scale-90 mb-6 flex flex-wrap transform shadow-lg transition-transform duration-300 ease-in-out text-black dark:text-wight mt-16 mb-16 p-6">
             <div class="mb-6 ml-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-3/12">
               <div class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20" data-te-ripple-init data-te-ripple-color="light">
@@ -85,7 +88,6 @@ function fetchAndDisplayArticles(category) {
               </div>
             </div>
           `;
-          
           articlesContainer.appendChild(articleElement);
           updateStarIcon(i); // Update star icon initially
         }
@@ -103,5 +105,4 @@ function fetchAndDisplayArticles(category) {
       articlesContainer.innerHTML = '<p>Error fetching articles</p>';
     });
 }
-
 fetchAndDisplayArticles('general');
