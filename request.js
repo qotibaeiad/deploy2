bbd8ece396e54657e067b8e944f89cb99bd89515
user <user@users-MacBook-Pro.local>
2024-02-19 16:42:50 +0200

    add netlify ip

--- request.js
+++ request.js
@@ -16,7 +16,7 @@ function fetchData() {
 }
 
 function toggleFavorite(i) {
-  checkvaforite[i] = !checkvaforite[i];
+  checkfavorite[i] = !checkfavorite[i];
   updateStarIcon(i);
 }
 
@@ -24,8 +24,8 @@ function updateStarIcon(i) {
   // Get the star icon element
   var starIcon = document.getElementById("staricon" + i);
 
-  // Update the SVG content based on the checkvaforite value
-  if (checkvaforite[i]) {
+  // Update the SVG content based on the checkfavorite value
+  if (checkfavorite[i]) {
     starIcon.innerHTML = `
       <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="absolute bottom-4 right-4 h-6 w-6 text-blue-500 dark:text-white star-icon">
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" fill="blue"></path>
