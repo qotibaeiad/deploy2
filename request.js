0cd1967b83d78b4f0b3a3f277cc1fa75b0c5e7ef
user <user@users-MacBook-Pro.local>
2024-02-19 14:35:56 +0200

    add netlify ip

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
 function fetchData() {
-    fetch('https://webtailwindserver.netlify.app/api/data')  // Replace with your Netlify app URL
+    fetch('https://tailwindserverweb.onrender.com/api/data')  // Replace with your Netlify app URL
       .then(response => {
         if (!response.ok) {
           throw new Error(`Network response was not ok: ${response.statusText}`);
