36ad30448171f80d959cc63064853e5e2b1ddb37
user <user@users-MacBook-Pro.local>
2024-02-21 01:18:38 +0200

    add search bar

--- request.js
+++ request.js
@@ -2,7 +2,7 @@ var serverurl = 'https://tailwindserverweb.onrender.com';
 
 
 
-function searchArticles() {
+async function searchArticles() {
   var searchTerm = document.getElementById('default-search').value;
   document.getElementById('loadingSpinner').classList.remove('hidden');
 
